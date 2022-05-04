48709e8ffebef2fdb20d0db13b5e920880d47cfb
hyperionhype <fredrik.ms12@gmail.com>
2022-05-04 14:49:00 +0700

    finish playRound function for all combinations possibility and make new variable for playRound parameters

--- app.js
+++ app.js
@@ -10,3 +10,29 @@ function computerPlay() {
 	}
 }
 console.log(computerPlay());
+
+let inputPlayer = 'Paper';
+let playerSelection = inputPlayer.toLowerCase();
+let computerSelection = computerPlay().toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === 'rocks' && computerSelection === 'rocks') {
+		return "it's a tie!";
+	} else if (playerSelection === 'paper' && computerSelection === 'paper') {
+		return "It's a tie!";
+	} else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+		return "It's a tie!";
+	} else if (playerSelection === 'rocks' && computerSelection === 'scissors') {
+		return 'You Win! Rocks beats Scissors!';
+	} else if (playerSelection === 'paper' && computerSelection === 'rocks') {
+		return 'You Win! Paper beats Rocks!';
+	} else if (playerSelection === 'scissors' && computerSelection === 'rocks') {
+		return 'You Win! Scissors beats Paper!';
+	} else if (playerSelection === 'scissors' && computerSelection === 'rocks') {
+		return 'You Lose! Rocks beats Scissors';
+	} else if (playerSelection === 'rocks' && computerSelection === 'paper') {
+		return 'You Lose! Paper beats Rocks!';
+	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		return 'You Lose! Scissors beats Paper!';
+	}
+}
