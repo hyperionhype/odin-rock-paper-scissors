288ddeee3469f43f5c009fa37f0035a638a6f7df
hyperionhype <fredrik.ms12@gmail.com>
2022-05-05 21:29:30 +0700

    finally i can record the game with every winner each round and decide which side the winner, all in console

--- app.js
+++ app.js
@@ -10,28 +10,46 @@ function computerPlay() {
 	}
 }
 
-let inputPlayer = window.prompt("What's your pick?", 'Rocks or Paper or Scissors');
-let playerSelection = inputPlayer.toLowerCase();
+let playerSelection = window.prompt("What's your pick?", 'Rocks or Paper or Scissors').toLowerCase();
 let computerSelection = computerPlay();
 
 let win = 'You Win!';
 let lose = 'You Lose!';
 let tie = "It's a tie!";
+let playerScore = 0;
+let computerScore = 0;
+let gameWinner = '';
+
+function gameWin() {
+	if (playerScore > computerScore) {
+		gameWinner = 'You Win!';
+		return gameWinner;
+	} else if (playerScore < computerScore) {
+		gameWinner = 'Computer Wins!';
+		return gameWinner;
+	}
+}
 
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === computerSelection) {
 		return `${tie}`;
 	} else if (playerSelection === 'rocks' && computerSelection === 'scissors') {
+		playerScore++;
 		return `${win}, Rocks beats Scissors!`;
 	} else if (playerSelection === 'paper' && computerSelection === 'rocks') {
+		playerScore++;
 		return `${win}, Paper beats Rocks!`;
 	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		playerScore++;
 		return `${win} Scissors beats Paper!`;
 	} else if (playerSelection === 'scissors' && computerSelection === 'rocks') {
+		computerScore++;
 		return `${lose} Rocks beats Scissors`;
 	} else if (playerSelection === 'rocks' && computerSelection === 'paper') {
+		computerScore++;
 		return `${lose} Paper beats Rocks!`;
 	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		computerScore++;
 		return `${lose}, Scissors beats Paper!`;
 	}
 }
@@ -41,9 +59,9 @@ function game() {
 		console.log(playRound(playerSelection, computerPlay()));
 		window.prompt("What's your pick?", 'Rocks or Paper or Scissors');
 	}
-	if (playerSelection === undefined) {
-		inputPlayer;
-	}
+	console.log('Player Score', playerScore);
+	console.log('Computer Score', computerScore);
+	console.log(gameWin());
 }
 
 game();
