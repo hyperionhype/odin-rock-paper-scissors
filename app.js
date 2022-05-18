0525289e78bd5ceb5fcd7026c45c3a44a66735b2
hyperionhype <fredrik.ms12@gmail.com>
2022-05-18 21:23:16 +0700

    adding new situation if the final score is tie so it will automatically start second game. The loop can only break if the game have a winner

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ let computerSelection = '';
 let playerSelection = '';
 let playerInput;
 let compInput;
-let win = 'You Win!'; // for playRound()
-let lose = 'You Lose!'; // for playRound()
-let tie = "It's a tie!"; // for playRound()
 let playerScore = 0; // value change on playRound()
 let computerScore = 0; // value change on playRound()
 let whoWin = ''; // value change on gameWinner()
@@ -25,10 +22,15 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+	//variable list
+	let win = 'You Win!';
+	let lose = 'You Lose!';
+	let tie = "It's a tie!";
 	computerSelection = computerPlay();
 	playerSelection = window.prompt("What's your pick?", 'Rocks or Paper or Scissors').toLowerCase();
 	playerInput = playerSelection;
 	compInput = computerSelection;
+	//end of variable list
 	if (playerSelection === computerSelection) {
 		return `${tie} You both pick ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}!`;
 	} else if (playerSelection === 'rocks' && computerSelection === 'scissors') {
@@ -59,6 +61,9 @@ function finaleScore() {
 	} else if (playerScore < computerScore) {
 		finalScore = 'For the Computer!';
 		return finalScore;
+	} else if (playerScore === computerScore) {
+		finalScore = 'For Both Side!'
+		return finalScore;
 	}
 }
 
@@ -77,15 +82,20 @@ function gameWinner() {
 
 function game() {
 	for (let i = 0; i < 5; i++) {
-		round++;
-		console.log(`Round ${round}`);
+		round++; // add 1 value every time  playRound()  gets call
+		console.log(`Round ${round}`); //checking round
 		console.log(playRound(playerSelection, computerSelection));
+		console.log(`Current score Player ${playerScore} - Comp ${computerScore}`);
+		console.log(`(Input check = You pick ${playerInput} - Computer pick ${compInput})`); //for checking input
+		console.log('--------'); //separator for each round
 	}
 	console.log(`Final Score: ${playerScore} - ${computerScore} ${finaleScore()}`);
 	console.log(gameWinner());
+
+	if (playerScore === computerScore) {
+		console.log("Let's go for second game! We need winner to end this loop!")
+		game();
+	}
 }
 
 game();
-
-//for checking input
-// console.log(`You pick ${playerInput} - Computer pick ${compInput}`);
